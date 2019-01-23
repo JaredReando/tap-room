0da22a6c619f57793a1ceb7d25a3c3cdcdbc9db4
Cole Marsteller and Jared Reando <jared@>
2019-01-23 10:41:54 -0800

    Initialize project and add simple UI for entering new kegs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Keg } from './models/keg.model'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  kegs: Keg[] = [
+    new Keg(3.4, 'banquet', 'coors', 3),
+    new Keg(6.8, 'Strong Blonde IPA', 'Pfreim', 5.50),
+    new Keg(1.2, 'BEER', 'Natty Ice', 1.25)
+  ]
+
+  someMethod(alcoholContent: number, beerName: string, beerBrand: string, beerPrice: number) {
+    let newKeg: Keg = new Keg(alcoholContent, beerName, beerBrand, beerPrice)
+
+    this.kegs.push(newKeg);
+  }
+
+
 }
---
+++ src/app/models/keg.model.ts
@@ -0,0 +1,5 @@
+export class Keg {
+  public pints: number = 124;
+  constructor(public alcoholContent: number, public name: string, public brand: string, public price: number) {
+  }
+}
